28994228942611ca284ae451e09fd556f65156cd
Amol K V <varadeamolk.1994@gmail.com>
2022-12-21 19:24:32 +0530

    map1

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
-import { useState } from "react";
-
 function App() {
-  // useState, hooks, Stateful Variable
-  // ES6 Syntax : De-Structureing
-  // let counter = 100;
-  let [counter, setCounter] = useState(100);
-
-  // Member function,
-  // ES6 Syntax: Arrow Function
-  // React Binding + using Interpolation
-  // How calling this function.
-  let increment = () => {
-    // logical opr
-    counter++;
-
-    // dom opr
-    setCounter(counter);
-  };
   let title = "Map Demo";
   let user = { id: 1, city: "mumbai" };
+  let list = ["mumbai", "delhi"];
 
   return (
     <div>
-      <h1>Counter Application</h1>
-      <h1> {counter} </h1>
-      <input type="button" value="Incrment" onClick={increment} />
       <h1> {title} </h1>
       <h1>{user.city}</h1>
+
+      {list.map((item) => (
+        <h1>{item}</h1>
+      ))}
     </div>
   );
 }
+
 export default App;
+
+// [delhi, mumbai] => [<h1>delhi</h1>, <h1>mumbai</h1>]
