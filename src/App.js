da33284b9010807d213a2d050f1c9948fadc57ff
Amol K V <varadeamolk.1994@gmail.com>
2022-12-21 19:07:44 +0530

    interpolation at attribute lavel

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 // USER DEFINED TAGS == COMPONENT
 function App() {
-  // PURE DATA MEMBERS
   let project = "My React Project";
   let username = "rahul";
+  let project1 = "Counter Application";
+  let btnValue = "Click Meeeeeee";
 
   // JSX => HTML CSS
   return (
     <div>
       <h1> {project} </h1>
       <h1>Hello {username} !</h1>
-      <h1>Hello {username} !</h1>
+      <input type="button" value={btnValue} />
     </div>
   );
 }
