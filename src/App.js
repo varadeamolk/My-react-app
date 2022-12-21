49578b5cf75632a5629e13dfa7ec7d60460be762
Amol K V <varadeamolk.1994@gmail.com>
2022-12-21 20:04:42 +0530

    dyanamic css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
+import { useState } from "react";
+
 function App() {
+  let [theme, setTheme] = useState("primary");
+
+  let makePrimaryTheme = () => {
+    theme = "primary";
+
+    setTheme(theme);
+  };
+
+  let makeSuccessTheme = () => {
+    theme = "success";
+
+    setTheme(theme);
+  };
+
+  let makeDangerTheme = () => {
+    theme = "danger";
+
+    setTheme(theme);
+  };
   return (
     <div>
-      <h1 className="bg-primary text-light p-1 fs-2">Bootstrap</h1>
+      <h1 className={`bg-${theme} text-light p-2`}>Bootstrap styling</h1>
 
-      <h1 className="bg-primary p-1 fs-2" style={{ color: "red" }}>
-        Bootstrap
-      </h1>
+      <input
+        className="btn btn-primary btn-sm me-1"
+        type="button"
+        value="Primary Theme"
+        onClick={makePrimaryTheme}
+      />
+      <input
+        className="btn btn-success btn-sm me-1"
+        type="button"
+        value="Success Theme"
+        onClick={makeSuccessTheme}
+      />
+      <input
+        className="btn btn-danger btn-sm me-1"
+        type="button"
+        value="Danger Theme"
+        onClick={makeDangerTheme}
+      />
     </div>
   );
 }
