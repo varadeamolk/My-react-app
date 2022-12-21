af70d2bcc05f3bc8c20bf6ca3fa62629f07763cc
Amol K V <varadeamolk.1994@gmail.com>
2022-12-21 19:28:47 +0530

    todo list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { useState } from "react";
 function App() {
   let title = "Map Demo";
-  let user = { id: 1, city: "mumbai" };
-  let list = ["mumbai", "delhi"];
-
+  let [list, setList] = useState([]);
+  let addItem = () => {
+    // alternate :: No DOM PLZ!
+    let textRef = document.querySelector("#textId1");
+    // logical part
+    let newList = [textRef.value, ...list];
+    // dom part
+    setList(newList);
+  };
   return (
     <div>
       <h1> {title} </h1>
-      <h1>{user.city}</h1>
-
+      <input type="text" name="" id="textId1" />
+      <input type="button" value="Add Item Into List" onClick={addItem} />
       {list.map((item) => (
         <h1>{item}</h1>
       ))}
