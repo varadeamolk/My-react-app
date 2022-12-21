91ebce0f0dbf443da96bd46aa70d3199f41fb89f
Amol K V <varadeamolk.1994@gmail.com>
2022-12-21 19:03:37 +0530

    basic interpolation

--- src/App.js
+++ src/App.js
@@ -1,46 +1,15 @@
-/* function App() {
-  return <h1>Hello</h1>;
-}
-
-export default App; */
-
-/* function App() {
-  return (
-    <h1>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum vero
-      tempore ducimus voluptate ad aliquam, expedita deleniti, odio nesciunt
-      voluptates officia ullam repellat quia repellendus! Autem, pariatur.
-      Voluptas, eos iure.
-    </h1>
-  );
-}
-
-export default App; */
-
+// USER DEFINED TAGS == COMPONENT
 function App() {
-  return (
-    <div>
-      <Person name="rahul" />
-      <Person name="panvel" />
-      <Person name="nashik" />
-      <Person name="mumbai" />
-      <Person name="pune" />
-    </div>
-  );
-}
+  // PURE DATA MEMBERS
+  let project = "My React Project";
+  let username = "rahul";
 
-//person tag
-function Person(name) {
+  // JSX => HTML CSS
   return (
     <div>
-      <h1>Person name</h1>
-      <img src="https://picsum.photos/400" alt="" />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
-        reiciendis modi eligendi blanditiis, nesciunt distinctio hic alias
-        quisquam eaque sint voluptatibus molestias excepturi veniam. Reiciendis,
-        omnis. Odio debitis fuga non?
-      </p>
+      <h1> {project} </h1>
+      <h1>Hello {username} !</h1>
+      <h1>Hello {username} !</h1>
     </div>
   );
 }
