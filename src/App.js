04c5acb951f1f3303cfa53b2cdc9896895558a89
Amol K V <varadeamolk.1994@gmail.com>
2022-12-21 19:13:25 +0530

    stateful counter

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
+import { useState } from "react";
+
 function App() {
-  // Data Members
-  let project = "Counter Application";
-  let btnValue = "Click Meeeeeee";
   // stateless variable
   let counter = 100;
+  // let counter = 10;
+  let [counter, setCounter] = useState(10);
 
-  // Member :: Type is Functions
-  let clickMe = () => alert();
-  let incrment = () => {};
-  let decrement = () => {};
   let increment = () => {
     counter = counter + 1;
     console.log(counter);
+
+    // RE-RENDER
+    setCounter(counter);
   };
 
-  // JSX
   return (
     <div>
-      <h1> {project} </h1>
-      <h1> </h1>
-      <input type="button" value={btnValue} onClick={clickMe} />
       <h1>Counter Application</h1>
-
-      <input type="button" value="Increment" onClick={incrment} />
       <h1>{counter}</h1>
-
       <input type="button" value="Increment" onClick={increment} />
     </div>
   );
