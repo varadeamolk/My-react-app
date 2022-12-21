9e42ab986313b4c4bea067ee12b2d6d10104b920
Amol K V <varadeamolk.1994@gmail.com>
2022-12-21 19:09:17 +0530

    member fun how to use

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-// USER DEFINED TAGS == COMPONENT
 function App() {
-  let project = "My React Project";
-  let username = "rahul";
-  let project1 = "Counter Application";
+  // Data Members
+  let project = "Counter Application";
   let btnValue = "Click Meeeeeee";
 
-  // JSX => HTML CSS
+  // Member :: Type is Functions
+  let clickMe = () => alert();
+  let incrment = () => {};
+  let decrement = () => {};
+
+  // JSX
   return (
     <div>
       <h1> {project} </h1>
-      <h1>Hello {username} !</h1>
       <input type="button" value={btnValue} />
+      <h1> </h1>
+      <input type="button" value={btnValue} onClick={clickMe} />
+
+      <input type="button" value="Increment" onClick={incrment} />
     </div>
   );
 }
