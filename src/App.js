37c0cdbbb8e09a1f0e0927a32b29ae8015e2c4f6
Amol K V <varadeamolk.1994@gmail.com>
2022-12-21 19:52:16 +0530

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-// import "./App.css";
 function App() {
   return (
     <div>
-      <h1 className="beautify">External Styling</h1>
+      <h1 className="bg-primary text-light p-1 fs-2">Bootstrap</h1>
+
+      <h1 className="bg-primary p-1 fs-2" style={{ color: "red" }}>
+        Bootstrap
+      </h1>
     </div>
   );
 }
