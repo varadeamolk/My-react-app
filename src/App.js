d0c49ebfa3d253217ffefa4ca13c02fa8003e84b
Amol K V <varadeamolk.1994@gmail.com>
2022-12-22 14:36:10 +0530

    read value from input

--- src/App.js
+++ src/App.js
@@ -1,40 +1,26 @@
+import { useState } from "react";
+
 function App() {
-  {
-    /** Default No param */
-  }
-  let clickMe1 = () => {
-    console.log("Hello");
-  };
+  let [message, setMessage] = useState("Hello");
 
-  let clickMe2 = (e) => {
-    console.log(e);
-  };
+  let updateMessage = (e) => {
+    // e.target.value=document.querySelector("input")
+    console.log(e.target.value);
 
-  let clickMe3 = (p1) => {
-    console.log(p1);
-  };
-
-  let clickMe4 = (e, p1) => {
-    console.log(e, p1);
+    setMessage(e.target.value);
   };
   return (
     <div>
-      <h1>click Demo</h1>
-      {/** Default No param */}
-      <input type="button" value="Click Me1" onClick={clickMe1} />
+      <h1>Working with input</h1>
 
-      {/** Default Event Param */}
-      <input type="button" value="Click Me2" onClick={clickMe2} />
-
-      {/** Custom Param */}
-      <input type="button" value="Click Me3" onClick={() => clickMe3("ram")} />
-
-      {/** Event + Custom Param */}
       <input
-        type="button"
-        value="Click Me4"
-        onClick={(e) => clickMe4(e, "shiv")}
+        type="text"
+        placeholder="Enter message"
+        value={message}
+        onChange={updateMessage} // this line is very important
       />
+
+      <h1>{message}</h1>
     </div>
   );
 }
