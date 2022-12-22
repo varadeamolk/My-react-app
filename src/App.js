50443b53fbe4f43e4a7f40c70cabf78f4c051c61
Amol K V <varadeamolk.1994@gmail.com>
2022-12-22 15:59:12 +0530

    map fun key prop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import { useState } from "react";
 
 function App() {
-  let [list, setList] = useState(["mumbai", "pune", "nashik"]);
+  let [list] = useState([
+    { message: "Hello", messageTime: new Date() },
+    { message: "Hi", messageTime: new Date() },
+    { message: "How are you", messageTime: new Date() },
+    { message: "I am fine", messageTime: new Date() },
+  ]);
 
   return (
     <div>
-      <h1>Map Demo</h1>
+      <h1 className="bg-primary text-light p-2 fs-2 ">Map Demo</h1>
 
-      {list.map((item) => (
-        <div className="alert alert-primary text-dark">{item}</div>
+      {list.map((item, index) => (
+        <div key={index}>
+          <div className="badge text-bg-primary ">
+            <div>
+              {item.message}
+              <span className="ms-5">
+                {item.messageTime.getUTCHours()}:{item.messageTime.getMinutes()}
+              </span>
+            </div>
+          </div>
+        </div>
       ))}
     </div>
   );
