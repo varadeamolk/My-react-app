3860519c54e53d5ebd768807e974e2ebc49e2984
Amol K V <varadeamolk.1994@gmail.com>
2022-12-22 16:13:56 +0530

    static message

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import { useState } from "react";
 
 function App() {
-  let [list] = useState([
+  let [list, setList] = useState([
     { message: "Hello", messageTime: new Date() },
     { message: "Hi", messageTime: new Date() },
     { message: "How are you", messageTime: new Date() },
     { message: "I am fine", messageTime: new Date() },
   ]);
 
+  // member function
+  let addMessage = () => {
+    let newMessage = { message: "Hello Everyone", messageTime: new Date() };
+    list = [newMessage, ...list];
+
+    setList(list);
+  };
+
   return (
     <div>
-      <h1 className="bg-primary text-light p-2 fs-2 ">Map Demo</h1>
+      <h1 className="bg-primary text-light p-2 fs-2 ">Messaging Demo</h1>
+      <div className="d-flex">
+        <input
+          className="form-control me-2"
+          type="text"
+          placeholder="Enter message"
+        />
+        <input
+          className="btn btn-primary btn-sm"
+          type="button"
+          value="Add Message"
+          onClick={addMessage}
+        />
+      </div>
 
       {list.map((item, index) => (
         <div key={index}>
