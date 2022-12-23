183fb59ab3dd9dd4dad9279a1234c6fcadf65e47
Amol K V <varadeamolk.1994@gmail.com>
2022-12-23 11:02:12 +0530

    code splitting, multiple component.

--- src/App.js
+++ src/App.js
@@ -1,89 +1,63 @@
-import axios from "axios";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 
 function App() {
-  let [title] = useState("API INTEGRATION");
-  let [messageList, setMessageList] = useState([]);
-  let [message, setMessage] = useState([]);
-  let inputRef = useRef();
-
-  let getAllMessages = async () => {
-    let url = `http://localhost:3001/message`;
-    let response = await axios.get(url);
-    // console.log(response);
-    // getting the messages from server
-    messageList = [...response.data];
-    // re-reendering data
-    setMessageList(messageList);
-  };
-
-  let handleOnChangeMessage = (e) => {
-    let message = e.target.value;
-
-    setMessage(message);
-
-    // or we can write this also  setMessage(e.target.value)
-  };
-
-  let createMessage = async () => {
-    let url = `http://localhost:3001/messages`;
-
-    // console.log(inputRef.current);
-    if (!inputRef.current.checkValidity()) {
-      alert("invalid message");
-      return;
-    }
-
-    let data = { message: message, messageTime: new Date() };
-    await axios.post(url, data);
-
-    getAllMessages(); // to refresh the content
-  };
-
-  let checkEnterCode = (e) => {
-    if (e.keyCode == 13) {
-      createMessage();
-    }
-  };
-
-  useEffect(() => {
-    // Spl Funcn :: Hook :: Like Constructor ::  Called while the Compoent is Initialized
-    getAllMessages();
-  }, []);
-
   return (
     <div>
-      <h1 className="bg-primary text-light fs-2 p-3">{title}</h1>
-      <div className="d-flex ">
-        <input
-          className="form-control"
-          type="text"
-          placeholder="Enter Message"
-          onChange={handleOnChangeMessage}
-          onKeyUp={checkEnterCode}
-          ref={inputRef}
-          required
-          minLength={3}
-        />
-        <input
-          className="btn btn-primary btn-sm"
-          type="button"
-          value="make ajax/post call"
-          onClick={createMessage}
-        />
-      </div>
+      <AppHeader />
+      <AppBody />
+      <AppBody />
+      <AppFooter />
+    </div>
+  );
+}
 
-      {messageList.map((item, index) => (
-        <div key={index}>
-          <div className="badge text-bg-primary opacity-75">
-            {item.message}
-            <span className="ms-4">
-              {new Date(item.messageTime).getHours()}:
-              {new Date(item.messageTime).getMinutes()}
-            </span>
+// <AppHeader />
+function AppHeader() {
+  return (
+    <div className="bg-warning text-light p-1">
+      <h1>Shopping App</h1>
+    </div>
+  );
+}
+
+function AppBody() {
+  let [list] = useState([{}, {}, {}, {}, {}, {}, {}, {}]);
+  return (
+    <div>
+      <div className="row">
+        {list.map((item, index) => (
+          <div className="col-12 col-md-3 my-2" key={index}>
+            <div className="card">
+              <img
+                src={`http://picsum.photos/${200 + index}`}
+                style={{ height: "300px", objectFit: "cover" }}
+                alt=""
+              />
+              <div className="card-header">Shoping Card</div>
+              <div className="card-body m">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+                asperiores libero minima delectus ducimus sint officia nihil non
+                modi quos, ut laborum illo velit magni hic consequatur ipsam
+                incidunt Lorem ipsum dolor sit amet consectetur, adipisicing
+                elit.
+              </div>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function AppFooter() {
+  return (
+    <div>
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{ height: "100px ", background: "lightblue" }}
+      >
+        showing@pp............
+      </div>
     </div>
   );
 }
