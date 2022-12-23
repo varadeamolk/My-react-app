c36da693f8f59095061b437550aae4140c606f43
Amol K V <varadeamolk.1994@gmail.com>
2022-12-23 15:08:15 +0530

    link the pages for easy nevigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<h1>Home</h1>}></Route>
-      <Route path="/home" element={<h1>Home</h1>}></Route>
-      <Route path="/explore" element={<h1>Explore</h1>}></Route>
-      <Route path="/*" element={<h1>Page not found</h1>}></Route>
-    </Routes>
+    <div>
+      <Link to={"/home"}>Home |</Link>
+      <Link to={"/explore"}>Explore |</Link>
+      <Link to={"/notifications"}>Notifications</Link>
+
+      <Routes>
+        <Route path="/home" element={<h1>Home</h1>}></Route>
+        <Route path="/notifications" element={<h1>Notifications</h1>}></Route>
+        <Route path="/explore" element={<h1>Explore</h1>}></Route>
+        <Route path="/*" element={<h1>Page not found</h1>}></Route>
+      </Routes>
+    </div>
   );
 }
 
