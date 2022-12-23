7dfd7ce0784193c9027abc797d39c4fb1624ac9f
Amol K V <varadeamolk.1994@gmail.com>
2022-12-23 15:20:25 +0530

    link the pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import AppHeader from "./components/AppHeader";
-import AppBody from "./components/AppBody";
-import AppFooter from "./components/AppFooter";
-
+import { Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div>
-      <AppHeader />
-      <AppBody />
-      <AppBody />
-      <AppFooter />
+      <Link to={"/home"}>Home |</Link>
+      <Link to={"/explore"}>Explore |</Link>
+      <Link to={"/notifications"}>Notifications</Link>
+
+      <Routes>
+        <Route path="/home" element={<h1>Home</h1>}></Route>
+        <Route path="/notifications" element={<h1>Notifications</h1>}></Route>
+        <Route path="/explore" element={<h1>Explore</h1>}></Route>
+        <Route path="/*" element={<h1>Page not found</h1>}></Route>
+      </Routes>
     </div>
   );
 }
